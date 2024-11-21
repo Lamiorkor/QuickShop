5eeb36734eea4ba61b4b068fe724ccf55ba84502
Naa Lamiorkor Boye <lamiorkor.boye@ashesi.edu.gh>
2024-11-21 22:21:43 +0000

    modified:   actions/register_action.php 	modified:   classes/user_class.php 	modified:   controllers/user_controller.php 	modified:   view/login_and_register.php

--- actions/register_action.php
+++ actions/register_action.php
@@ -1,16 +1,14 @@
 <?php
 include "../controllers/user_controller.php";
 
-//session_start();
+session_start();
 
 if (isset($_POST['signup'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    // $requested_role = $_POST['rolerequest'];
-    // // var_dump($requested_role);
-    // // exit();
-    // $user_id = $_SESSION['user_id'];
+    $requested_role = $_POST['rolerequest'];
+    $user_id = $_SESSION['user_id'];
 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
@@ -18,13 +16,20 @@ if (isset($_POST['signup'])) {
     $registerUser = registerController($name, $email, $hashed_password);
 
     // Call addRequestController
-    //$role_request = addRequestController($user_id, $requested_role);
+    $role_request = addRequestController($user_id, $requested_role);
 
     // Check if registration was successful
     if ($registerUser !== false) {
+        if($role_request !== null) {
         // Redirect to login page with success message
         header("Location:../view/login_and_register.php");
         exit();
+        } else {
+            // Redirect to registration page with error message
+            echo "Request failed. Please try again.";
+            header("Location:../view/login_and_register.php");
+            exit();
+        }
     } else {
         // Redirect to registration page with error message
         echo "Registration failed. Please try again.";
