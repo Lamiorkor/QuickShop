9b5375e4f28b81a4ed0868e100678508afae8a28
Naa Lamiorkor Boye <lamiorkor.boye@ashesi.edu.gh>
2024-11-21 21:07:31 +0000

    modified:   .DS_Store 	modified:   actions/register_action.php 	modified:   classes/user_class.php 	modified:   controllers/user_controller.php 	modified:   css/login_and_register.css 	modified:   db/infosec_assign2.sql 	modified:   view/login_and_register.php

--- actions/register_action.php
+++ actions/register_action.php
@@ -1,17 +1,21 @@
 <?php
 include ('../controllers/user_controller.php');
 
-if (isset($_POST['signup'])) {
+session_start();
 
+if (isset($_POST['signup'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $requested_role = $_POST['rolerequest'];
 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
      // Call registerController
      $registerUser = registerController($name, $email, $hashed_password);
 
+     // Call addRequestController
+     $role_request = addRequestController($_SESSION['user_id'], $requested_role);
 
     // Check if registration was successful
     if ($registerUser !== false) {
